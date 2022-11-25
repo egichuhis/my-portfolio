45a07fa42b3c513a2a4c4d968e25f63ed317b6e7
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-11-25 11:04:28 +0300

    Update files

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+let menuOpen = false;
+hamburgerMenu.addEventListener('click', () => {
+    if (!menuOpen) {
+        hamburgerMenu.classList.add('open');
+        menuOpen = true;
+    } else {
+        hamburgerMenu.classList.remove('open');
+        menuOpen = false;
+    }
+    mobileMenu.classList.toggle('open');
+    return menuOpen;
+});
+const reviewPage = document.querySelectorAll('.nav-item');
+for (let i = 0; i < reviewPage.length; i += 1) {
+    reviewPage[i].addEventListener('click', () => {
+        hamburgerMenu.classList.toggle('open');
+        mobileMenu.classList.toggle('open');
+    });
+}
