47400d04bdca47fa748f7322feb5caaa1a11b1a1
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-11-26 13:38:20 +0300

    Add popup menu styling

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-let menuOpen = false;
-hamburgerMenu.addEventListener('click', () => {
-    if (!menuOpen) {
-        hamburgerMenu.classList.add('open');
-        menuOpen = true;
-    } else {
-        hamburgerMenu.classList.remove('open');
-        menuOpen = false;
-    }
-    mobileMenu.classList.toggle('open');
-    return menuOpen;
-});
-const reviewPage = document.querySelectorAll('.nav-item');
-for (let i = 0; i < reviewPage.length; i += 1) {
-    reviewPage[i].addEventListener('click', () => {
-        hamburgerMenu.classList.toggle('open');
-        mobileMenu.classList.toggle('open');
-    });
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const menuItems = document.querySelector('.mobile-menu');
+
+hamburgerMenu.addEventListener('click', displayMenu);
+
+function displayMenu() {
+    menuItems.classList.remove('mobile-menu-view');
+    
 }
+
+// const mobileMenu = document.querySelector('.mobile-menu');
+// let menuOpen = false;
+// hamburgerMenu.addEventListener('click', () => {
+//     if (!menuOpen) {
+//         hamburgerMenu.classList.add('open');
+//         menuOpen = true;
+//     } else {
+//         hamburgerMenu.classList.remove('open');
+//         menuOpen = false;
+//     }
+//     mobileMenu.classList.toggle('open');
+//     return menuOpen;
+// });
+// const reviewPage = document.querySelectorAll('.nav-item');
+// for (let i = 0; i < reviewPage.length; i += 1) {
+//     reviewPage[i].addEventListener('click', () => {
+//         hamburgerMenu.classList.toggle('open');
+//         mobileMenu.classList.toggle('open');
+//     });
+// }
