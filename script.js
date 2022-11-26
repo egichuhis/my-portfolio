3d13a9724f74607ebeef7a76b7dd9394a0304d02
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-11-26 14:12:28 +0300

    Remove commented out code

--- script.js
+++ script.js
@@ -19,24 +19,3 @@ for (let i = 0; i < menuItemsList.length; i++) {
         menuItems.classList.toggle('mobile-menu-view');
     })
 }
-
-// const mobileMenu = document.querySelector('.mobile-menu');
-// let menuOpen = false;
-// hamburgerMenu.addEventListener('click', () => {
-//     if (!menuOpen) {
-//         hamburgerMenu.classList.add('open');
-//         menuOpen = true;
-//     } else {
-//         hamburgerMenu.classList.remove('open');
-//         menuOpen = false;
-//     }
-//     mobileMenu.classList.toggle('open');
-//     return menuOpen;
-// });
-// const reviewPage = document.querySelectorAll('.nav-item');
-// for (let i = 0; i < reviewPage.length; i += 1) {
-//     reviewPage[i].addEventListener('click', () => {
-//         hamburgerMenu.classList.toggle('open');
-//         mobileMenu.classList.toggle('open');
-//     });
-// }
